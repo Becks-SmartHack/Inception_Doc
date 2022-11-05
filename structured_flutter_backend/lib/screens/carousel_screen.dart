8394ead4a65ca893eb8690b1554b1af12024b20a
DenisaTerchescu <dterchescu@yahoo.com>
2022-11-05 21:58:37 +0200

    Carousel screen done

--- structured_flutter_backend/lib/screens/carousel_screen.dart
+++ structured_flutter_backend/lib/screens/carousel_screen.dart
@@ -17,61 +17,93 @@ class _CarouselScreenState extends State<CarouselScreen> {
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text("Carousel screen"),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
+      resizeToAvoidBottomInset : false,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            CarouselSlider(
-              options: CarouselOptions(
-                height: height/2,
-                enlargeCenterPage: false,
-                viewportFraction: 2.0
+            Container(
+              child: Image.asset('assets/Component3.png'),
+              alignment: Alignment.topLeft,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 50, 10, 50),
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: height/2,
+                  enlargeCenterPage: true,
+                  viewportFraction: 2.0,
+                ),
+                items: [0,1,2].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                              width: MediaQuery.of(context).size.width/1.2,
+                              margin: EdgeInsets.symmetric(horizontal: 10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.lightBlueAccent.withOpacity(0.3),
+                                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                              ),
+                              child:
+                                Column(
+                                 mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+                                      child: Text(
+                                          "Doctor",
+                                      style: TextStyle(
+                                        fontSize:25,
+                                      ),),
 
-              ),
-              items: [1,2,3].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                            width: MediaQuery.of(context).size.width/1.2,
-                            margin: EdgeInsets.symmetric(horizontal: 10.0),
-                            decoration: BoxDecoration(
-                                color: Colors.black12,
-                                borderRadius: BorderRadius.all(Radius.circular(30)),
-                            ),
-                            child:
-                              GestureDetector(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(18.0),
-                                  child: Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-
-                                ),
-                                onTap: () {
-                                  Navigator.push<Widget>(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => AppFeaturesScreen(),
                                     ),
-                                  );}
+                                    GestureDetector(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(15.0),
+                                        child: imgList[i],
+                                      ),
+                                      onTap: () {
+                                        Navigator.push<Widget>(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => AppFeaturesScreen(),
+                                          ),
+                                        );}
+                                    ),
 
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                          "50€",
+                                      style: TextStyle(
+                                       fontSize: 20
+                                      ),),
+                                    ),
+                                  ],
 
-                              )
-                        );
-
-                  },
-                );
-              }).toList(),
-            )
+                                )
+                          );
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+            // Padding(padding: EdgeInsets.fromLTRB(0, 118, 0, 0)),
+            Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomRight,
+                  child: Image.asset('assets/Component4.png'),
+                ),
+              ),
           ],
         ),
-      ),
-
-    );
+      );
 
   }
 }
+
+List imgList = [
+  Image.asset('assets/1663779989005 1.png'),
+  Image.asset('assets/consult-rezidenti-blog- 1.png'),
+  Image.asset('assets/spital-generic-shutterstock 1.png'),
+];
