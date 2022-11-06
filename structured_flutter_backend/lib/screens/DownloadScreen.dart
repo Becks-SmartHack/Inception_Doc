968bbb963e77151f3130a4aa4e73f9f64987ca72
DenisaTerchescu <dterchescu@yahoo.com>
2022-11-06 02:44:13 +0200

    Connected the screens till the download one

---
+++ structured_flutter_backend/lib/screens/DownloadScreen.dart
@@ -0,0 +1,69 @@
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class DownloadScreen extends StatefulWidget {
+  const DownloadScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DownloadScreen> createState() => _DownloadScreenState();
+}
+
+class _DownloadScreenState extends State<DownloadScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    final double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: Image.asset('assets/Component3.png'),
+              alignment: Alignment.topLeft,
+            ),
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                        "Application generated successfully!",
+                        style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+              ],
+            ),
+
+            Expanded(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center, children: [
+                Image.asset(
+                    'assets/Asset 5@4x.png',
+                    width: width/2),
+
+              ]),
+            ),
+
+
+            Container(
+              width: width,
+              alignment: Alignment.bottomRight,
+              child: Stack(children: [
+                Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  Image.asset('assets/Component4.png'),
+                ]),
+              ]),
+            ),
+          ],
+        )
+    );
+
+  }
+}
+
